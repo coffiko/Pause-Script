30797545774a1c61cda604e519a0f39474d65bac
coffiko <116488972+coffiko@users.noreply.github.com>
2022-10-23 23:34:07 +0500

    Add files via upload

---
+++ gogame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gogame : MonoBehaviour
+{
+   public GameObject panel;
+
+    public void continuegame()
+    { 
+        Time.timeScale = 1f;
+        panel.SetActive(false);       
+    }
+}
---
+++ pause.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+   public GameObject panel;
+
+    public void pausegame()
+    { 
+        Time.timeScale = 0f;
+        panel.SetActive(true);       
+    }
+}
